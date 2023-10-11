b24730777501b7d908eab4a62b75198b774b93f5
Jonathan Loy <jonrloy@gmail.com>
2023-10-10 22:29:27 -0400

    Redoing the party member and action menus

--- battle.gd
+++ battle.gd
@@ -4,31 +4,58 @@ signal textbox_closed
 enum BarType {HEALTH, ENERGY}
 
 @export var enemy : Resource = null
-var current_player_health = 0
+@export var party : Array[Resource] = [null, null, null]
+var current_party_health = [0, 0, 0]
+var players = []
 var current_enemy_health = 0
 var is_defending = false
+var targeted_player = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	current_player_health = State.current_health
-	current_enemy_health = enemy.health
-	set_bar($PCPanel/PanelContainer/PCData/PCHealthBar, BarType.HEALTH, current_player_health, State.max_health)
-	set_bar($EnemyContainer/EnemyHealthBar, BarType.HEALTH, current_enemy_health, enemy.health)
-	$EnemyContainer/Enemy.texture = enemy.texture
+	set_party_display()
+	set_enemy_display()
+	
 	$TextboxMarginContainer/Textbox.hide()
-	$ActionsMarginContainer/ActionPanel.hide()
+	$ActionsMarginContainer.hide()
 	
 	display_text("A wild %s appears!" % enemy.name.to_upper())
 	await self.textbox_closed
-	$ActionsMarginContainer/ActionPanel.show()
+	$ActionsMarginContainer.show()
 
-func set_bar(progress_bar, type, current, max):
-	progress_bar.value = current
-	progress_bar.max_value = max
+func set_party_display():
+	for n in party.size():
+		current_party_health[n] = party[n].current_health
+	
+	players.append($PartyPanel/PartyMemberContainer1.get_node("MarginContainer/PartyMemberData/GridContainer/HPContainer"))
+	players.append($PartyPanel/PartyMemberContainer2.get_node("MarginContainer/PartyMemberData/GridContainer/HPContainer"))
+	players.append($PartyPanel/PartyMemberContainer3.get_node("MarginContainer/PartyMemberData/GridContainer/HPContainer"))
+
+	for n in party.size():
+		set_bar(players[n], BarType.HEALTH, current_party_health[n], party[n].max_health)	
+		#set_bar(players[n], BarType.ENERGY, current_party_health[n], party[n].max_health)
+	$PartyPanel/PartyMemberContainer1.get_node("MarginContainer/PartyMemberData/GridContainer/Name").text = party[0].name
+	$PartyPanel/PartyMemberContainer2.get_node("MarginContainer/PartyMemberData/GridContainer/Name").text = party[1].name
+	$PartyPanel/PartyMemberContainer3.get_node("MarginContainer/PartyMemberData/GridContainer/Name").text = party[2].name
+	
+	$PartyPanel/PartyMemberContainer1.get_node("MarginContainer/PartyMemberData/Portrait").texture = party[0].portrait	
+	$PartyPanel/PartyMemberContainer2.get_node("MarginContainer/PartyMemberData/Portrait").texture = party[1].portrait
+	$PartyPanel/PartyMemberContainer3.get_node("MarginContainer/PartyMemberData/Portrait").texture = party[2].portrait
+
+func set_enemy_display():
+	current_enemy_health = enemy.health	
+	set_bar($EnemyContainer, BarType.HEALTH, current_enemy_health, enemy.health)
+	$EnemyContainer/Enemy.texture = enemy.texture
+		
+func set_bar(player_data, type, current, maximum):
 	if type == BarType.HEALTH:
-		progress_bar.get_node("Label").text = "HP: %d/%d" % [current, max]
+		player_data.get_node("HealthBar").value = current
+		player_data.get_node("HealthBar").max_value = maximum
+		player_data.get_node("HP").text = "HP: %d" % [current]
 	elif type == BarType.ENERGY:
-		progress_bar.get_node("Label").text = "EN: %d/%d" % [current, max]
+		player_data.get_node("EnergyBar").value = current
+		player_data.get_node("EnergyBar").max_value = maximum
+		player_data.get_node("Energy").text = "EN: %d" % [current]
 	
 func _input(event):
 	if $TextboxMarginContainer/Textbox.visible and (Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
@@ -55,8 +82,8 @@ func enemy_turn():
 		display_text("You block the attack!")
 		await self.textbox_closed
 	else:	
-		current_player_health = max(0, current_player_health - enemy.attack_damage)
-		set_bar($PCPanel/PanelContainer/PCData/PCHealthBar, BarType.HEALTH, current_player_health, State.max_health)
+		current_party_health[targeted_player] = max(0, current_party_health[targeted_player] - enemy.attack_damage)
+		set_bar(players[targeted_player], BarType.HEALTH, current_party_health[targeted_player], party[targeted_player].max_health)
 		
 		$AnimationPlayer.play("screen_shake")
 		await $AnimationPlayer.animation_finished
@@ -74,12 +101,13 @@ func _on_attack_pressed():
 	display_text("You swing valiantly at %s!" % enemy.name)
 	await self.textbox_closed
 	
-	current_enemy_health = max(0, current_enemy_health - State.attack_damage)
-	set_bar($EnemyContainer/EnemyHealthBar, BarType.HEALTH, current_enemy_health, enemy.health)
+	var num = randi_range(0, party.size()-1)
+	current_enemy_health = max(0, current_enemy_health - party[num].attack_damage)
+	set_bar($EnemyContainer, BarType.HEALTH, current_enemy_health, enemy.health)
 	
 	$AnimationPlayer.play("enemy_damaged")
 	await $AnimationPlayer.animation_finished
-	display_text("You dealt %d damage!" % State.attack_damage)
+	display_text("You dealt %d damage!" % party[num].attack_damage)
 	
 	if current_enemy_health == 0:
 		display_text("%s is defeated!" % enemy.name)
---
+++ src/Player.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+@export var name : String = "Player"
+@export var portrait : Texture = null
+@export var texture : Texture = null
+@export var current_health = 35
+@export var max_health = 35
+@export var attack_damage = 20
+
